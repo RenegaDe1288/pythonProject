9fc7180b2f36e7133533e1e8506ea79845863075
MSI <D121188@yandex.ru>
2020-12-02 23:52:25 +0300

    2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from sys import platform
+import platform
+import sys
 info = 'OS info is \n{}\n\nPython version is {} {}'.format(
     platform.uname(), sys.version, platform.architecture())
 print(info)

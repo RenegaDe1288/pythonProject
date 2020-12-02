901093f068382a04d9e91b8e870e54334219cd12
MSI <D121188@yandex.ru>
2020-12-02 23:48:42 +0300

    1

---
+++ main.py
@@ -0,0 +1,7 @@
+from sys import platform
+info = 'OS info is \n{}\n\nPython version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+print(info)
+
+with open('os_info.txt', 'w') as ff:
+    ff.write(info)

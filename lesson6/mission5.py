0d8724cd036d32dd8fe6f60187db847e20d9d6cb
MSI <D121188@yandex.ru>
2021-04-29 12:09:02 +0300

    done

--- lesson6/mission5.py
+++ lesson6/mission5.py
@@ -1,4 +1,4 @@
-# 1 решение без цикла
+      # 1 решение без цикла
 ticket = int(input('Введите номер билетика:'))
 part_1 = ticket % 10 + (ticket // 10) % 10 + (ticket // 100) % 10
 part_2 = ticket // 100000 + (ticket // 10000) % 10 + (ticket // 1000) % 10
@@ -7,7 +7,7 @@ if part_2 == part_1:
 else:
     print('Неудача')
 
-# 2 решение с циклом и обнулением
+      # 2 решение с циклом и обнулением
 ticket = int(input('Введите номер билетика:'))
 summ_part_1 = 0
 summ_part_2 = 0
@@ -30,23 +30,20 @@ if summ_part_2 == summ_part_1:
 else:
     print('Неудачник')
 
-# 3 решение с циклом
+      # 3 решение с циклом
 
 ticket = int(input('Введите номер билетика:'))
 summ_part_1 = 0
 summ_part_2 = 0
 n = 0
 while n < 6:
+    number = ticket % 10
+    ticket = ticket // 10
+    n += 1
     if n < 3:
-        number = ticket % 10
         summ_part_1 += number
-        ticket = ticket // 10
-        n += 1
     if n >= 3:
-        number = ticket % 10
         summ_part_2 += number
-        ticket = ticket // 10
-        n += 1
 print('Вторая половинка =', summ_part_1)
 print('Первая половинка  = ', summ_part_2)
 if summ_part_2 == summ_part_1:

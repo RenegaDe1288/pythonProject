3a26e7dbd8b4fd60975fe4b28ecd042f226c5d69
MSI <D121188@yandex.ru>
2021-04-30 12:41:05 +0300

    done

--- lesson8/mission02.py
+++ lesson8/mission02.py
@@ -1,7 +1,17 @@
 cells = 1
 total_hours = int(input('Введите коичество часов: '))
-for hour in range(1, total_hours // 3 + 1):
+for hour in range(1, total_hours + 1, 3):
     hour *= 3
+    cells *= 2
+    print('Прошло часов: ', hour)
+    print('Клеток = ', cells)
+    print()
+print('всего прошло =', hour, 'часов. Клеток = ', cells, 'Осталось часов = ', total_hours - hour)
+
+
+cells = 1
+total_hours = int(input('Введите коичество часов: '))
+for hour in range(3, total_hours + 3, 3):
     cells *= 2
     print('Прошло часов: ', hour)
     print('Клеток = ', cells)

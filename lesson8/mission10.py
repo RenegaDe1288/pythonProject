c4e931c0bc6148145ede86cffe5571ea32b54dac
MSI <D121188@yandex.ru>
2021-04-30 20:04:53 +0300

    done

--- lesson8/mission10.py
+++ lesson8/mission10.py
@@ -6,41 +6,40 @@ print('Мальчиков =', boys)
 print('Девочек = ', girls)
 if girls -  boys != 0 and (boys-girls > 2 or girls - boys > 2):
     print('Рассадить детей невозможо!')
-elif boys == girls:
-    for place in range (1, girls + boys+1):
+
+for place in range(1, girls + boys + 1):
+    if boys == girls:
         if place%2 == 1:
             print('b')
         else:
             print('g')
-elif boys - girls == 1:
-    for place in range (1, girls + boys):
-        if place%2 == 1:
-            print('b')
-        else:
-            print('g')
-    print('b')
-elif girls - boys == 1:
-    for place in range (1, girls + boys):
-        if place%2 == 1:
-            print('g')
-        else:
-            print('b')
-    print('g')
-elif girls - boys == 2:
-    for place in range (1, girls + boys):
-        if place%2 == 1:
-            print('g')
-        else:
-            print('b')
-        if place == 3:
-            print('g')
-elif boys - girls == 2:
-    for place in range (1, girls + boys):
+
+for place in range(1, girls + boys ):
+    if boys - girls == 1:
         if place%2 == 1:
             print('b')
         else:
             print('g')
         if place == 3:
             print('b')
-
-
+    elif girls - boys == 1:
+        if place%2 == 1:
+            print('g')
+        else:
+            print('b')
+        if place == 3:
+            print('g')
+    elif girls - boys == 2:
+        if place%2 == 1:
+            print('g')
+        else:
+            print('b')
+        if place == 3:
+            print('g')
+    elif boys - girls == 2:
+        if place%2 == 1:
+            print('b')
+        else:
+            print('g')
+        if place == 3:
+            print('b')

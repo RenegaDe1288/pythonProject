924dfaa186fc4e530fa30156f1456f29a24fbe38
MSI <D121188@yandex.ru>
2021-04-30 21:11:13 +0300

    done

--- lesson8/mission10.py
+++ lesson8/mission10.py
@@ -1,45 +1,23 @@
 import random
 
 boys = random.randint(7, 9)
-girls = random.randint(7, 11)
+girls = random.randint(7, 20)
 print('Мальчиков =', boys)
 print('Девочек = ', girls)
-if girls - boys != 0 and (boys - girls > 2 or girls - boys > 2):
+
+answer = ''
+if girls > 2 * boys or boys > 2 * girls:
     print('Рассадить детей невозможо!')
-
-for place in range(1, girls + boys + 1):
-    if boys == girls:
-        if place % 2 == 1:
-            print('b')
-        else:
-            print('g')
-
-for place in range(1, girls + boys):
-    if boys - girls == 1:
-        if place % 2 == 1:
-            print('b')
-        else:
-            print('g')
-        if place == 3:
-            print('b')
-    elif girls - boys == 1:
-        if place % 2 == 1:
-            print('g')
-        else:
-            print('b')
-        if place == 3:
-            print('g')
-    elif girls - boys == 2:
-        if place % 2 == 1:
-            print('g')
-        else:
-            print('b')
-        if place == 3:
-            print('g')
-    elif boys - girls == 2:
-        if place % 2 == 1:
-            print('b')
-        else:
-            print('g')
-        if place == 3:
-            print('b')
+elif girls > boys:
+    k = girls - boys
+    for gbg in range(k):
+        answer += 'gbg'
+    for gb in range(1, girls - k):
+        answer += 'gb'
+elif girls < boys:
+    k = boys - girls
+    for bgb in range(k):
+        answer += 'bgb'
+    for gb in range(1, boys - k):
+        answer += 'bg'
+print(answer)

b965eb0371235c0948e95d827a4608b583870d53
MSI <D121188@yandex.ru>
2021-05-02 02:07:29 +0300

    done

--- lesson10/mission1.py
+++ lesson10/mission1.py
@@ -1,5 +1,5 @@
 num = int(input('Введите число  '))
-for a in range(num+1):
-    for b in range(0, 11, 2):
-        print(b + a, end='\t')
+for a in range(1, num+1):
+    for b in range(1, a):
+        print(b, end='\t')
     print()

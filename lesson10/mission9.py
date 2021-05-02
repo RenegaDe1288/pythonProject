0001328a5ea81e769289aec3d4d2473b044cc4aa
MSI <D121188@yandex.ru>
2021-05-02 18:46:45 +0300

    done

--- lesson10/mission9.py
+++ lesson10/mission9.py
@@ -3,10 +3,9 @@ num =0
 
 print('Сначала было так\n')
 for y in range(n):
-    num = y
     for x in range(n):
         if x >= n - y-1:
-            print(y+num+1, end='  ')
+            print(num+1, end='  ')
             num += 2
         else:
             print(' ', end=' ')

bcb047f86053ba6ae4e5156084fda4a7da9a4fe9
MSI <D121188@yandex.ru>
2021-05-02 14:14:01 +0300

    done

--- lesson10/mission5.py
+++ lesson10/mission5.py
@@ -1,11 +1,12 @@
 count = 0
 lent = int(input('Введите количество запросов:'))
-for num in range(lent):
-    number = int(input('Введите число:'))
+for num in range(1, lent+1):
+    print('Введите число:', num, end=' ')
+    number = int(input())
     max = 0
     while number >0:
         count +=1
         max += 1
         number = number//10
-    print('Число сотсоит из ', max, 'цифр')
+    print('Число состоит из ', max, 'цифр')
 print('Общее количество цифр в последовательности = ', count)

71d9b9b39a284821cd61ae1ffbde6a1c7299da6c
MSI <D121188@yandex.ru>
2021-05-06 17:13:59 +0300

    done

---
+++ lesson11/mission5.py
@@ -0,0 +1,9 @@
+import math
+earth = 10.8321*10**11
+planet= float(input('введите радиус Случайной планеты: '))
+v_plan = math.pi*(4/3)*planet**3
+print(v_plan)
+if v_plan>earth:
+    print('Объём планеты Земля больше в ', round(v_plan/earth, 3), 'раз')
+else:
+    print('Объём планеты Земля меньше в ', round(earth/v_plan, 3), 'раз')

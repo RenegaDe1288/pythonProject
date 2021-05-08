45963a067f7b2ae292d6132c130ff466e688c8f5
MSI <D121188@yandex.ru>
2021-05-08 15:57:05 +0300

    done

---
+++ lesson12/mission08.py
@@ -0,0 +1,9 @@
+def information(name, surname, contry, town, street, house, flat):
+    print('\nИмя = ', name, '\nФамилия = ', surname, '\nСтрана = ', contry,
+          '\nГород = ', town, '\nУлица = ', street, '\nДом = ', house, '\nКвартира = ', flat)
+    print()
+
+
+information("Вася", 'Ложкин', 'Россия', 'Подольск', 'Северная', 6, 156)
+information("Петя", "Пукин", 'Россия', 'Подольск', 'Северная', 6, 106)
+information("Лох", "Лохович", 'Россия', 'Подольск', 'Северная', 6, 66)

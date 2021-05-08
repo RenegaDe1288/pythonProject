82fb51ba84e276d8a4979cddf868da4995b7df14
MSI <D121188@yandex.ru>
2021-05-08 16:41:59 +0300

    done

---
+++ lesson12/mission1.py
@@ -0,0 +1,12 @@
+
+
+def summa_n():
+    summ = 0
+    for num in range (1, n + 1):
+        summ += num
+    print('Я знаю, что сумма чисел от ', 1, 'до ',5, 'равна ', summ)
+
+
+while True:
+    n = int(input('Введите число: '))
+    summa_n()

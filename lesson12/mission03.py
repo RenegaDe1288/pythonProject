7d64139f39447f697da5ce2bc0538ea3411d0bbe
MSI <D121188@yandex.ru>
2021-05-08 12:30:35 +0300

    done

---
+++ lesson12/mission03.py
@@ -0,0 +1,12 @@
+def information():
+    name = input("Введите имя: ")
+    surname = input("введите фамилию: ")
+    street = input('Введите улицу: ')
+    house = input('Введите дом: ')
+    print('\nИмя = ', name, '\nФамилия = ', surname, '\nУлица = ', street, '\nДом = ', house)
+    print()
+
+information()
+information()
+information()
+

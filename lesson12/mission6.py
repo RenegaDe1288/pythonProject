901d37b01b02f604eb12d027b11c788c3da7a676
MSI <D121188@yandex.ru>
2021-05-08 19:12:13 +0300

    done

--- lesson12/mission6.py
+++ lesson12/mission6.py
@@ -1,12 +1,14 @@
-def coordinates():
-    x = int(input('Введите координату икс: '))
-    y = int(input('Введите координату игрек: '))
+x = int(input('Введите координату икс: '))
+y = int(input('Введите координату игрек: '))
+
+
+def coordinates(x, y):
     print('Координаты: ', x, y)
     if -1 <= x <= 1 and -1 <= y <= 1:
         print('Монетка где-то рядом. ')
     else:
         print('Монетки рядом НЕТ.')
 
-coordinates()
+coordinates(x, y)
 
 

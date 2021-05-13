d3db94e0f6aa840b7d1d625f31e5036c1e283ad4
MSI <D121188@yandex.ru>
2021-05-13 23:52:23 +0300

    done

--- lesson13/mission4.py
+++ lesson13/mission4.py
@@ -1,35 +1,23 @@
-string = input('Введте число: ')
-mantisse = ''
-method = ''
-print(string)
-
-
-def reverse(number):
-    x2 = 0
-    while number > 0:
-        digit = number % 10
-        number = number // 10
-        x2 = x2 * 10
-        x2 = x2 + digit
-    return x2
-
-
-for symbol in string:
-    if symbol == "*":
-        break
-    else:
-        mantisse += symbol
+def operation(string):
+    str = ''
+    for symbol in string:
+        if symbol == "*":
+            break
+        else:
+            str += symbol
+    return str
 
 
 def reverse(string):
     string = string[::-1]
     return string
 
-string = reverse(string)
 
-for symbol in string:
-    if symbol == "*":
-        break
-    else:
-        method += symbol
+string = input('Введте число: ')
+print(string)
+mantisse = operation(string)
+string = reverse(string)
+method = operation(string)
+
+
 print('Должно  выглядеть так: \nМантисса = ', mantisse, '\nПорядок = ', reverse(method))

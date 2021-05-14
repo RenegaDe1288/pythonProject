e96be3756ad710a136b2517257d14a94af39ed62
MSI <D121188@yandex.ru>
2021-05-14 15:51:32 +0300

    done

--- lesson13/mission10.py
+++ lesson13/mission10.py
@@ -6,10 +6,10 @@ def count(s, a, i):
         main = a - pers
         # print('коэффициэнт = ', k)
         print('\nОстаток долга = ', s)
-        print('         общая сумма погашения за год ', year, ' = ', round(a, 2))
-        print('Выплачено процентов = ', round(pers, 2))
-        print('Выплачено тело кредита = ', round(main, 2))
-        s = round(s - main,2)
+        print('         общая сумма погашения за год ', year, ' = ', round(a, 3))
+        print('Выплачено процентов = ', round(pers, 3))
+        print('Выплачено тело кредита = ', round(main, 3))
+        s = round(s - main,3)
         year += 1
     return s
 
@@ -31,7 +31,7 @@ s = count(s, a, i)
 print('\nОстаток долга = ', s)
 
 i = 0.1
-new = 2
+new = 4
 new = n - 3 + new
 a = ak(s, i, new)
 s = count(s, a, i)

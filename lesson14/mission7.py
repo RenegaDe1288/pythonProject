b5df65289bc29618d866f65245eed845d56ef200
MSI <D121188@yandex.ru>
2021-05-14 21:04:00 +0300

    done

--- lesson14/mission7.py
+++ lesson14/mission7.py
@@ -1,16 +1,15 @@
 x = int(input('ВВедите первый год: '))
 y = int(input('ВВедите второй год: '))
 
-for num in range(x, y+1):
+for num in range(x, y + 1):
     num = str(num)
     count = 0
     y = num[3]
     for sym in num:
         if sym == y:
-            count+=1
+            count += 1
         if count == 3:
             print(num)
 
-
-y = str(1909%10)
+y = str(1909 % 10)
 print(type(y))

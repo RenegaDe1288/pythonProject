6070e9144b650f3822f16a8ab97f3e0b14fcdb65
MSI <D121188@yandex.ru>
2021-05-14 21:12:47 +0300

    done

--- lesson14/mission7.py
+++ lesson14/mission7.py
@@ -4,11 +4,14 @@ y = int(input('ВВедите второй год: '))
 for num in range(x, y + 1):
     num = str(num)
     count = 0
-    y = num[3]
+    count2 = 0
     for sym in num:
-        if sym == y:
+        if sym == num[3]:
             count += 1
-    if count == 3:
+        if sym == num[0]:
+            count2 += 1
+
+    if count == 3 or count2 == 3:
         print(num)
 
 y = str(1909 % 10)

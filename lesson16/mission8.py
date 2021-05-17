b23cc1b6c9ef3853ca9063ace764a680b84147ba
MSI <D121188@yandex.ru>
2021-05-17 17:38:00 +0300

    done

--- lesson16/mission8.py
+++ lesson16/mission8.py
@@ -1,14 +1,16 @@
 my_list = [1, 2, 3, 4, 5]
-start = 1
+start = 0
 while len(my_list) > 1:
-    if start == len(my_list):
-        start = 0
+    print('Начало счета с номера: ', my_list[start])
     for num in range(start, len(my_list)):
         number = int(input('Выбывает человек под номером: '))
         start = my_list.index(number)
         my_list.remove(number)
         break
-    print('Начало счета с номера: ', my_list[start-1])
+    if start == len(my_list):
+        start = 0
+    if len(my_list) == 1:
+        break
     print('Остались игроки: ', my_list)
 
 print('Победитель: ', my_list)

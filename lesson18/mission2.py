3cf96736fbd2e49864e66274444678a73190ff01
MSI <D121188@yandex.ru>
2021-05-21 16:47:34 +0300

    done

--- lesson18/mission2.py
+++ lesson18/mission2.py
@@ -1,6 +1,6 @@
 text = input('Введите текст ').split()
 maximum = ''
 for word in text:
-    if len(word) > len(max):
+    if len(word) > len(maximum):
         maximum = word
-print('Слово с максимальным количеством букв: {} , букв = {}'.format(max, len(max)))
+print('Слово с максимальным количеством букв: {} , букв = {}'.format(maximum, len(maximum)))

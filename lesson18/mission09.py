9b01010fdb5c64680605550188a882c81efcac65
MSI <D121188@yandex.ru>
2021-05-21 15:09:33 +0300

    done

--- lesson18/mission09.py
+++ lesson18/mission09.py
@@ -1,8 +1,13 @@
 text = input('Введите сообщение: ')
 
 num = [1 if letter.islower() else 0 for letter in text]
-text_1 = ''.join([letter.lower() if num.count(1) >= num.count(0)
-                  else letter.upper()
-                  for letter in text])
+if num.count(1) >= num.count(0):
+    text = text.lower()
+else:
+    text = text.upper()
 
-print(text_1)
+# text_1 = ''.join([letter.lower() if num.count(1) >= num.count(0)
+#                   else letter.upper()
+#                   for letter in text])
+
+print(text)

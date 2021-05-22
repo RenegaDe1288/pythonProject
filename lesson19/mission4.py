7d996a569ef46804195e28eb64a3490196468a60
MSI <D121188@yandex.ru>
2021-05-22 19:02:08 +0300

    done

---
+++ lesson19/mission4.py
@@ -0,0 +1,36 @@
+goods = {
+    'Лампа': '12345',
+    'Стол': '23456',
+    'Диван': '34567',
+    'Стул': '45678',
+}
+
+store = {
+    '12345': [
+        {'quantity': 27, 'price': 42},
+    ],
+    '23456': [
+        {'quantity': 22, 'price': 510},
+        {'quantity': 32, 'price': 520},
+    ],
+    '34567': [
+        {'quantity': 2, 'price': 1200},
+        {'quantity': 1, 'price': 1150},
+    ],
+    '45678': [
+        {'quantity': 50, 'price': 100},
+        {'quantity': 12, 'price': 95},
+        {'quantity': 43, 'price': 97},
+    ],
+}
+
+for i in goods:
+    summa = 0
+    total_quantity = 0
+    for n in store[goods[i]]:
+        summa += n['quantity'] * n['price']
+        total_quantity += n['quantity']
+    print('Сумма товаров {} , всего отсаток {} = {}'.format(summa, i, total_quantity))
+
+
+

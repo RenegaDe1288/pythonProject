71e30a1c9624ee09cd73bd6dd299f7bd88300e88
MSI <D121188@yandex.ru>
2021-05-23 12:50:35 +0300

    done

---
+++ lesson19/mission6.py
@@ -0,0 +1,18 @@
+n = int(input('Введите количество пар: '))
+
+sinonims = {}
+
+for i in range(n):
+    list = input('Введите пару слов через тире: ').split('-')
+    for i in list:
+        sinonims[i] = list[list.index(i)-1]
+print(sinonims)
+
+while True:
+    word = input('Введите слово: ')
+    if word in sinonims:
+        print('Синоним слова {} это {}'.format(word, sinonims[word]))
+    else:
+        print('Такого слова в словаре нет.')
+
+

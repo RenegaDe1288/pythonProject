bb1f6192ca925b6a6a4d9d3aa82de944e4c4b4d6
MSI <D121188@yandex.ru>
2021-07-21 17:17:53 +0300

    done

--- lesson24/mission02.py
+++ lesson24/mission02.py
@@ -4,22 +4,23 @@ class Monitor:
     res = 'WQHD'
     freq = 60
 
+
 class Headphones:
     name = 'Sony'
     sensitivity = 108
     micro = False
 
-monitor_1= Monitor()
-monitor_2= Monitor()
-monitor_2.freq=144
-monitor_3= Monitor()
+
+monitor_1 = Monitor()
+monitor_2 = Monitor()
+monitor_2.freq = 144
+monitor_3 = Monitor()
 monitor_3.freq = 70
-monitor_4= Monitor()
+monitor_4 = Monitor()
 print(monitor_1.freq)
 print(monitor_2.freq)
 print(monitor_3.freq)
 print(monitor_4.freq)
-
 
 headphones_1 = Headphones()
 headphones_2 = Headphones()

4eeba81dc05c791e4c0042cc0d31a80faf2ebb2d
MSI <D121188@yandex.ru>
2021-07-27 00:26:04 +0300

    done

--- lesson24/mission8.py
+++ lesson24/mission8.py
@@ -1,29 +1,25 @@
-import  random
+import random
 
 
 class Cards:
 
-
-    def __init__(self,name,points):
+    def __init__(self, name, points):
         self.name = name
         self.points = points
 
 
 class Gamers:
 
-
     def __init__(self):
         self.player_points = 0
         self.dealer_points = 0
 
     def info(self):
-        print('У Игрока {}  очков. У Дилера {} очков '.format(self.player_points,self.dealer_points))
-
+        print('У Игрока {}  очков. У Дилера {} очков '.format(self.player_points, self.dealer_points))
 
     def pl_count(self, card):
         self.player_points += card.points
-        print('У Игрока выпала  {}  и всего  {} очков '.format(card.name,self.player_points))
-
+        print('У Игрока выпала  {}  и всего  {} очков '.format(card.name, self.player_points))
 
     def dl_count(self, card):
         self.dealer_points += card.points
@@ -36,18 +32,14 @@ class Gamers:
             return False
 
 
-
-
-name_list = ['Двойка','Тройка','Четверка','Пятерка','Шестерка','Семерка','Восьмерка','Девятка','Десятка']
-list_cards = [Cards(name,num+2) for num, name in enumerate(name_list)]
+name_list = ['Двойка', 'Тройка', 'Четверка', 'Пятерка', 'Шестерка', 'Семерка', 'Восьмерка', 'Девятка', 'Десятка']
+list_cards = [Cards(name, num + 2) for num, name in enumerate(name_list)]
 list_cards.append(Cards('Дама', 10))
 list_cards.append(Cards('Валет', 10))
 list_cards.append(Cards('Король', 10))
 list_cards.append(Cards('Туз', 11))
 
-
 player = Gamers()
-
 
 player.pl_count(random.choice(list_cards))
 player.pl_count(random.choice(list_cards))
@@ -59,34 +51,30 @@ while player.check():
     if answer == 1:
         player.pl_count(random.choice(list_cards))
         if player.player_points > 21:
-            print('Победил Дилер с очками  {} \n Игрок програл с очками {}'.format(player.dealer_points,player.player_points))
+            print('Победил Дилер с очками  {} \n Игрок програл с очками {}'.format(player.dealer_points,
+                                                                               player.player_points))
             break
-        else:
-            player.info()
+    player.info()
     answer2 = int(input('Дилеру: Хотите взять еще карту? 1/2 :  '))
     if answer2 == 1:
         player.dl_count(random.choice(list_cards))
         if player.dealer_points > 21:
-            print('Победил Игрок с очками  {} \n Дилер програл с очками {}'.format(player.player_points,player.dealer_points))
+            print('Победил Игрок с очками  {} \n Дилер програл с очками {}'.format(player.player_points,
+                                                                                   player.dealer_points))
             break
-    elif answer and answer2 == 2 :
+    elif answer and answer2 == 2:
         player.info()
         if player.player_points > player.dealer_points:
-            print('Победил       Игрок с очками  {} \n Дилер програл с очками {}'.format(player.player_points,player.dealer_points))
+            print('Победил       Игрок с очками  {} \n Дилер програл с очками {}'.format(player.player_points,
+                                                                                         player.dealer_points))
+            break
+
+        elif player.player_points < player.dealer_points:
+            print('Победил      Дилер с очками  {} \n Игрок програл с очками {}'.format(player.dealer_points,
+                                                                                        player.player_points))
+
+            break
         else:
-            print('Победил      Дилер с очками  {} \n Игрок програл с очками {}'.format(player.dealer_points,player.player_points))
-        break
+            print('Ничья', player.info())
     elif player.player_points and player.dealer_points == 21:
-        print('Ничья',player.info())
-
-
-
-
-
-
-
-
-
-
-
-
+        print('Ничья', player.info())

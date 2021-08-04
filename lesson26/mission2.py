da194e123cc006fc462644177736ad2ef7b5d9a3
MSI <D121188@yandex.ru>
2021-08-05 00:22:01 +0300

    done

--- lesson26/mission2.py
+++ lesson26/mission2.py
@@ -7,7 +7,7 @@ list_2 = [3, 8, 4, 9]
 
 for a, b in product(list_1, list_2):
     if (a * b) == 56:
-        print('Найдены числа: ', a, b)
+        print('Найдены числа :', a, b)
 
 """ Решение №2 через генераторное выражение"""
 
@@ -23,7 +23,7 @@ list_3 = (search(i, y) for i in list_1 for y in list_2)
 
 for i in list_3:
     if isinstance(i, tuple):
-        print('Найдены числа: ', i[0], i[1])
+        print('Найдены числа :', i[0], i[1])
 
 """Решение №3 через генератор - функцию"""
 
